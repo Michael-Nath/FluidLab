47e3dca0b2345832bfab5475bfb2c07e1e14f991
Michael-Nath <michael.d.nath@gmail.com>
2023-05-26 10:49:40 -0700

    Coded up basic gcbc architecture

--- fluidlab/models/gc_bc.py
+++ fluidlab/models/gc_bc.py
@@ -2,11 +2,15 @@
 
 import torch
 import torch.nn as nn
+import torch.optim as optim
+import torch.distributions as distributions
 from dataloader import TrajectoryDataset
+import itertools
 
 
 class ConvModule(nn.Module):
     def __init__(self, input_channels, output_channels, kernel, stride, pad):
+        super().__init__()
         self.conv = nn.Conv2d(
             in_channels=input_channels, 
             out_channels=output_channels, 
@@ -21,6 +25,7 @@ class ConvModule(nn.Module):
 
 class MLPModule(nn.Module):
     def __init__(self, input_dim, output_dim):
+        super().__init__()
         self.l1 = nn.Linear(input_dim, output_dim)
         self.bn = nn.BatchNorm1d(output_dim)
         self.relu = nn.ReLU()
@@ -30,27 +35,51 @@ class MLPModule(nn.Module):
 class GCBCAgent(nn.Module):
     def build_conv_layers_(self) -> nn.Sequential:
         # We will take in a concatenation of the current scene obs and a goal configuration 
-        conv_module_one = ConvModule(6, 32, 3, 1, 1)
+        conv_module_one = ConvModule(6, 32, 5, 1, 1)
         conv_module_two = ConvModule(32, 64, 3, 1, 0)
-        conv_module_three = ConvModule(64, 128, 3, 1, 0)
+        conv_module_three = ConvModule(64, 64, 3, 3, 0)
         return nn.Sequential(
-            ("conv_module_one", conv_module_one),
-            ("conv_module_two", conv_module_two),
-            ("conv_module_three", conv_module_three),
+            conv_module_one,
+            conv_module_two,
+            conv_module_three
         )
         
     def build_mlp_layers_(self) -> nn.Sequential:
-        l1 = MLPModule(-1, 128)
+        flatten = nn.Flatten()
+        l1 = MLPModule(64 * 84 * 84, 128)
         l2 = MLPModule(128, 128)
         l3 = MLPModule(128, self.ac_dim) # output a mean vector corresponding to the number of action components
         return nn.Sequential(
-            ("linear_one", l1),
-            ("linear_two", l2),
-            ("linear_three", l3)
+            flatten,
+            l1,
+            l2,
+            l3
         )
     def __init__(self, ac_dim):
+        super().__init__()
         self.ac_dim = ac_dim
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.logstd = nn.Parameter(torch.zeros((ac_dim), device=self.device))
         self.conv_layers = self.build_conv_layers_()
         self.mlp_layers = self.build_mlp_layers_()
-    def forward(self, x):
-        return self.mlp_layers(self.conv_layers(x))
+        self.gcbc_optim = optim.Adam(
+            itertools.chain(
+            self.conv_layers.parameters(), 
+            self.mlp_layers.parameters(), 
+            [self.logstd]
+            )
+        )
+    def forward(self, x) -> distributions.Distribution:
+        means = self.mlp_layers(self.conv_layers(x)).to(self.device)
+        return distributions.Normal(means, torch.exp(self.logstd))
+    def update(self, obs_no, ac_na):
+        dist = self.forward(obs_no)
+        pred_ac_na = dist.sample()
+        return pred_ac_na.to(self.device)
+    
+agent = GCBCAgent(5)
+ex_input_obs_batch = torch.randn((5, 3, 256, 256))
+ex_goal_obs_batch = torch.randn((5, 3, 256, 256))
+ex_ac_batch = torch.randn((5, 5))
+inpt = torch.cat((ex_input_obs_batch, ex_goal_obs_batch), 1)
+print(agent.update(inpt, ex_ac_batch))
